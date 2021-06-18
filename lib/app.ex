80b27b91180a257470979c5e615b1ee7a5df7538
Celeo <mattboulanger@fastmail.com>
2021-06-18 13:52:46 -0700

    Move the listener to app.ex

--- lib/app.ex
+++ lib/app.ex
@@ -21,3 +21,26 @@ defmodule IRC.Application do
     Supervisor.start_link(children, opts)
   end
 end
+
+defmodule IRC.Listener do
+  require Logger
+
+  @listen_args [:binary, packet: :line, active: true, reuseaddr: true]
+
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port, @listen_args)
+    Logger.info("Starting listener on port #{port}")
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    {:ok, pid} =
+      DynamicSupervisor.start_child(IRC.DynamicSupervisor, {IRC.ClientConnection, {client}})
+
+    :ok = :gen_tcp.controlling_process(client, pid)
+
+    loop_acceptor(socket)
+  end
+end
