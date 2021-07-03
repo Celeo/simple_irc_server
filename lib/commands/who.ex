510eb3167ac90bdb6568359ba4ad432a4510bcf6
Celeo <mattboulanger@fastmail.com>
2021-07-02 18:39:51 -0700

    Remove extra comments

--- lib/commands/who.ex
+++ lib/commands/who.ex
@@ -2,22 +2,11 @@ defmodule IRC.Commands.Who do
   require Logger
   @behaviour IRC.Commands.Base
 
-  # << WHO cel*
-  # >> :platinum.libera.chat 352 Celeo * ~Celeo user/celeo platinum.libera.chat Celeo H :0 Celeo
-  # >> :platinum.libera.chat 352 Celeo * sid97751 id-97751.brockwell.irccloud.com tungsten.libera.chat celphi G :0 rybka
-  # >> :platinum.libera.chat 352 Celeo * ~celeste1m 2001:470:69fc:105::3ea9 zirconium.libera.chat celeste1 H :0 @celeste1:matrix.org
-  # >> :platinum.libera.chat 352 Celeo * ~slep cpc150002-brnt4-2-0-cust437.4-2.cable.virginm.net osmium.libera.chat slep G :0 Celeste
-  # >> :platinum.libera.chat 352 Celeo * celelibi user/celelibi cadmium.libera.chat Celelibi H :0 Alors, il est beau mon whois ?
-  # >> :platinum.libera.chat 315 Celeo cel* :End of /WHO list.
-
   @impl IRC.Commands.Base
   def run(_parameters, client_state, server_state) do
     # TODO implement search
     # TODO respect +i
     # TODO 315 RPL_ENDOFWHO reply at end when needed
-
-    # IO.inspect(server_state.clients)
-    # IO.inspect(Map.to_list(server_state.clients))
 
     Task.start(fn ->
       Enum.each(Map.to_list(server_state.clients), fn {nick, client_pid} ->
